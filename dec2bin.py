a95782f3f1b3cd8ddf8c2482fdc6a0961ce19cde
Rohan100898 <rohanruhal@gmail.com>
2025-03-16 23:08:36 +0530

    Create dec2bin.py

---
+++ dec2bin.py
@@ -0,0 +1,12 @@
+# convert decimal to binary
+
+num = 250
+temp = num
+bin = ""
+while (num!=0):
+  r = int(num%2)
+  num = int(num/2)
+  bin = bin+str(r)
+
+bin = bin[::-1] #reverse the string
+print(bin)
