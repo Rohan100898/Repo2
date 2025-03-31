9cb840f4070fbde7ec2ed552cbd913b42c3d4e86
Rohan100898 <rohanruhal@gmail.com>
2025-03-31 18:19:46 +0530

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -4,7 +4,8 @@ def test_add():
   assert add(10,5) == 15
 
 def test_substract():
-  assert substract(10,5) == 5
+  assert substract(10,5) = 7 # wrong value
+#  assert substract(10,5) == 5
 
 def test_multiply():
   assert multiply(10,5) == 50
